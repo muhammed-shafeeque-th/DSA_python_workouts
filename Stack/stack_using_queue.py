8e0163f352095362c41a781df2989b23a40e485b
shafeeque <shafeeqsha06@gmail.com>
2024-09-28 17:58:27 +0530

    added workout on stack using queue

--- Stack/stack_using_queue.py
+++ Stack/stack_using_queue.py
@@ -2,45 +2,42 @@ class Stack:
     """This class has stack implementation with list with size delimiter"""
     def __init__(self, size: int) -> None:
         """Initialize the empty stack"""
-        self._stack = []
-        self._extra_stack = []
+        self._queue = []
+        self._extra_queue = []
         self._size = size
 
     def push(self, value) -> None:
         """Push value to stack"""
-        if self._size < len(self._stack):
+        if self._size < len(self._queue):
             raise Exception('stack overflow')
-        self._stack.append(value)
+        self._extra_queue.append(value)
+        while self._queue:
+            self._extra_queue.append(self._queue.pop(0))
+        while self._extra_queue:
+            self._queue.append(self._extra_queue.pop(0))
 
     def pop(self):
         """Pop element from stack and returns it"""
-        if len(self._stack) == 0:
+        if len(self._queue) == 0:
             raise Exception('Can not pop from empty stack')
-        elm = self._stack.pop(0)
-        while self._stack:
-            self._extra_stack.append(self._stack.pop(0))
-
+        elm = self._queue.pop(0)
         return elm
-
-
-        return self._stack.pop()
-
 
     def peak(self):
         """Return last element in stack """
-        return self._stack[-1]
+        return self._queue[0]
 
     def size(self) -> int:
         """Returns size of the stack"""
-        return len(self._stack)
+        return len(self._queue)
 
     def is_empty(self) -> bool:
         """Return True if stack is empty else False"""
-        return len(self._stack) == 0
+        return len(self._queue) == 0
 
 
-    def get_stack(self):
+    def get_queue(self):
         """Return protected stack for testing purpose """
-        return self._stack
+        return self._queue
 
 
