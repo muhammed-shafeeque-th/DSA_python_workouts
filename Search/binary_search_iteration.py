1f898a149f32e918ca0e00a4e6a239d740a68ebb
shafeeque <shafeeqsha06@gmail.com>
2024-09-30 21:35:23 +0530

    added binary search algo using iteration method

---
+++ Search/binary_search_iteration.py
@@ -0,0 +1,20 @@
+
+def binary_search(arr: list, value: int) -> int:
+    """
+    -Accepts a sorted array returns the position of the value
+    -Returns -1 if the value is not found.
+     """
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if value == arr[mid]:
+            return mid
+        elif value < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
+
+
---
+++ Search/binary_search_iteration.test.py
@@ -0,0 +1,27 @@
+import unittest
+from binary_search_iteration import *
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self._arr = [1, 2, 3, 4, 6, 7, 9, 12, 14, 16, 22, 25, 35, 66, 77, 78, 80, 88, 90]
+
+    def test_for_non_existed_value(self):
+        self.assertEqual(binary_search(self._arr, 888), -1,
+                         'Search for non existed value in arr')
+
+    def test_for_value(self):
+        self.assertEqual(binary_search(self._arr, 3), 2,
+                         'Search for  value in arr')
+
+    def test_for_fist_value(self):
+        self.assertEqual(binary_search(self._arr, 1), 0,
+                         'Search for the fist value in arr')
+
+    def test_for_last_value(self):
+        self.assertEqual(binary_search(self._arr, 90), len(self._arr)-1,
+                         'Search for the last value in arr')
+
+if __name__ == '__main__':
+    unittest.main()
