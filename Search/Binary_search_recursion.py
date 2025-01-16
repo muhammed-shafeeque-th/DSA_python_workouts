9ca2c053e543bdb0cd6ffeb19cab62072816b1b6
Muhammed Shafeeque th <shafeeqsha06@gmail.com>
2025-01-16 10:17:01 +0530

    Update Binary_search_recursion.py

--- Search/Binary_search_recursion.py
+++ Search/Binary_search_recursion.py
@@ -3,10 +3,13 @@ arr = [1, 3, 5, 6, 7, 8, 9, 10, 11]
 def binaryRecursionSearch(arr, target, left, right):
     if left > right:
         return print('value does not found in the list')
+    
     mid = (left + right) // 2
     if arr[mid] == target:
+        return True
+    elif arr[mid] < target:
         binaryRecursionSearch(arr, target, mid + 1, right)
-    elif arr[mid] > target:
+    else:
         binaryRecursionSearch(arr, target, left, mid-1)
 
 
